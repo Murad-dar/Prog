20dde37246d66d66937cfbd3fe61cd13b3fc9de2
user <user@debian-BULLSEYE-live-builder-AMD64>
2024-10-21 14:39:33 +0500

    1234

---
+++ lab2for.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+int main (){
+    double h, x = 0;
+    FILE *graph;
+    printf("Enter h:");
+    scanf("%lf", &h);
+    graph = fopen("graphik.txt", "w");
+    for (double x = 0; x <= 2; x = x + h){
+        if(x <= 1){
+            fprintf(graph, "%lf\t%lf\n", x, cos(x + pow(x,3)));
+            printf("%lf\t%lf\n", x, cos(x + pow(x,3)));
+        }else{
+            fprintf(graph, "%lf\t%lf\n", x, exp(pow(-x,2)) - pow(x,2) + 2*x);
+            printf("%lf\t%lf\n", x, exp(pow(-x,2)) - pow(x,2) + 2*x );
+        }
+    }
+    fclose(graph);
+    return 0;
+}
